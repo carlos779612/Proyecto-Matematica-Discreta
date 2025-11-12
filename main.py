960bbf3c757653c25c7d08ec6f7fdced36f611bd
EstudianteUC <estudianteUC@UCUXB100215.continental.edu.pe>
2025-11-12 13:34:55 -0500

    Modifiqué main.py para prueba
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ if __name__ == "__main__":
     nombre = input("¿Tu nombre? ")
     print(saludo(nombre))
     print("cambio en github")
+<<<<<<< HEAD
     print("actualizacion de Luis Angel")
+=======
+    print("hans") 
+    print("Hola desde GitHub y la rama ramaprueba")
+
+>>>>>>> 0153a29 (Modifiqué main.py para prueba)
