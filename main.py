13acf592986aa37435cf67f75b3ff9f6c8f5be32
fran <61085108@continental.edu.pe>
2025-11-26 12:53:11 -0500

    cambio final ejer 1

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ from matplotlib.patches import FancyArrowPatch
 
 
 # === Definición del DFA ===
-states = {"q0","q1","q2","q3","q4"}
+states = {"q0","q1","q2","q3","q4","q5"}
 alphabet = {"a","b"}
-delta = {("q0","a"):"q1",
-        ("q1","a"):"q4",
-        ("q1","b"):"q4",
-        ("q2","a"):"q0",
-        ("q2","b"):"q1",
-        ("q3","b"):"q2",
-        ("q4","a"):"q4",
-        ("q4","b"):"q4"}
+delta = {
+    ("q0","a"):"q1",
+    ("q1","b"):"q2",
+    ("q2","b"):"q3",
+    ("q3","b"):"q4",
+    ("q1","a"):"q5",
+    ("q2","a"):"q5",
+    ("q3","a"):"q5",
+    ("q4","a"):"q5",
+    ("q4","b"):"q5",
+    ("q5","a"):"q5",
+    ("q5","b"):"q5",
+}
 
 
 q0, F = "q0", {"q4"}
